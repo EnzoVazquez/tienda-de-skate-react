c6ff689c3ee5b4dd228032e26944020af729e644
enzo vazquez <enzov7511@gmail.com>
2022-01-26 15:17:15 -0300

    Cambios NavBar

---
+++ tienda_skate/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+//import {logo} from '.images/logo.png'
+
+export default function NavBar(){
+    return(
+        <div> 
+            <ul>
+                <h1>POST MORTEM</h1>
+                <a>TABLAS</a>
+                <a>TRUCKERS</a>
+                <a>RULEMANES</a>
+                <a>LIJAS</a>
+            </ul>
+        </div>
+  )  
+}
