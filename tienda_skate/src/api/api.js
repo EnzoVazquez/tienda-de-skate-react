96bd6665aac2dfde8c99b9a386b36218a080cd77
enzo vazquez <enzov7511@gmail.com>
2022-02-16 11:34:21 -0300

    arreglos

--- tienda_skate/src/api/api.js
+++ tienda_skate/src/api/api.js
@@ -5,18 +5,21 @@ const products = [
         title:'Tabla SANTA CRUZ MANDALA',
         price: 15999,
         pictureUrl:'https://drifters.com.ar/uploads/product_image/24025/DriftersPDP_HRD_SC00000641_Shot1.jpg',
+        category: 'tabla'
     },
     {
         id: 2,
         title:'Tabla CLIVER TIME FOR PINK',
         price: 7600,
-        pictureUrl:'https://drifters.com.ar/uploads/product_image/21928/DriftersPDP_HRD_CLE0001675_Shot1.jpg'
+        pictureUrl:'https://drifters.com.ar/uploads/product_image/21928/DriftersPDP_HRD_CLE0001675_Shot1.jpg',
+        category: 'tabla'
     },
     {
         id: 3,
         title:'Tabla WOODOO BH STRAWBERRY',
         price:7700,
-        pictureUrl:'https://drifters.com.ar/uploads/product_image/24827/DriftersPDP_HRD_SK16001694-0000_Shot1_v2.jpg'
+        pictureUrl:'https://drifters.com.ar/uploads/product_image/24827/DriftersPDP_HRD_SK16001694-0000_Shot1_v2.jpg',
+        category: 'tabla'
     },
 ]
 const promesa = new Promise(function(resolve, rejected){
--- tienda_skate/src/components/ItemDetail.js
+++ tienda_skate/src/components/ItemDetail.js
@@ -1,11 +1,11 @@
 import'./ItemDetail.css'
 
-export default function ItemDetail({item}){
+export default function ItemDetail(product){
     return(
         <div>
-            <img src={item.pictureUrl}></img>
-            <p>{item.title}</p>
-            <h2>$ {item.price}</h2>
+            <img src={product.pictureUrl}></img>
+            <p>{product.title}</p>
+            <h2>$ {product.price}</h2>
             <button>Comprar</button>
         </div>
     )
